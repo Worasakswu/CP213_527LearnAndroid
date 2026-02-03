25d4191974a71a6b4d3d1aee62afe26bfa647616
Salm0nlover <worasak.sankla@g.swu.ac.th>
2026-02-03 16:46:32 +0700

    labupdate1

--- lab/app/src/main/java/com/example/a527lablearnandroid/MainActivity.kt
+++ lab/app/src/main/java/com/example/a527lablearnandroid/MainActivity.kt
@@ -1,72 +1,143 @@
-package com.example.a527lablearnandroid
+package com.example.lablearnandroind
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.example.a527lablearnandroid.ui.theme._527LabLearnAndroidTheme
+import androidx.compose.ui.unit.sp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            _527LabLearnAndroidTheme {
-                MainScreen()
+            Column(modifier = Modifier.fillMaxSize()
+                .background(color = Color.Gray)
+                .height(32.dp)
+                .padding(32.dp)) {
+//hp
+                Box(modifier = Modifier
+                    .height(32.dp)
+                    .fillMaxWidth()
+                    .background(color = Color.Red)
+                )
+                {
+                    Text(
+                        text = "HP",
+                        modifier = Modifier.align(alignment = Alignment.CenterStart)
+                            .fillMaxWidth(fraction = 0.62f)
+                            .background(color = Color.Green )
+                            .padding(8.dp)
+                    )
+                }
+
+//image
+                Image(
+                    painter = painterResource(id = com.example.a527lablearnandroid.R.drawable.nick),
+                    contentDescription = "nick",
+                    modifier = Modifier
+                        .size(500.dp)
+                        .align(Alignment.CenterHorizontally)
+                        .padding(32.dp)
+                        .clickable {
+                            startActivity(Intent(this@MainActivity,ListActivity::class.java))
+                        }
+
+                )
+                var str by remember { mutableStateOf(8) }
+                var agi by remember { mutableStateOf(8) }
+                var int by remember { mutableStateOf(8) }
+//status
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                )
+                {
+
+                    Column {
+                        Button(onClick = { str++ }) {
+                            Image(
+                                painter = painterResource(com.example.a527lablearnandroid.R.drawable.baseline_arrow_upward_24)
+                                , contentDescription = "up"
+                            )
+
+                        }
+                        Text(modifier = Modifier.align(Alignment.CenterHorizontally),text = "Str", fontSize = 20.sp)
+                        Text(modifier = Modifier.align(Alignment.CenterHorizontally),text = str.toString(), fontSize = 20.sp)
+                        Button(onClick = { str-- }) {
+                            Image(
+                                painter = painterResource(com.example.a527lablearnandroid.R.drawable.outline_arrow_downward_24)
+                                , contentDescription = "down"
+                            )
+
+                        }
+                    }
+                    Column {
+                        Button(onClick = { agi++ }) {
+                            Image(
+
+                                painter = painterResource(com.example.a527lablearnandroid.R.drawable.baseline_arrow_upward_24)
+                                , contentDescription = "up"
+                            )
+
+                        }
+                        Text(modifier = Modifier.align(Alignment.CenterHorizontally),text = "Agi", fontSize = 20.sp)
+                        Text(modifier = Modifier.align(Alignment.CenterHorizontally),text = agi.toString(), fontSize = 20.sp)
+                        Button(onClick = { agi-- }) {
+                            Image(
+                                painter = painterResource(com.example.a527lablearnandroid.R.drawable.outline_arrow_downward_24)
+                                , contentDescription = "down"
+                            )
+
+                        }
+                    }
+                    Column {
+                        Button(onClick = { int++ }) {
+                            Image(
+                                painter = painterResource(com.example.a527lablearnandroid.R.drawable.baseline_arrow_upward_24)
+                                , contentDescription = "up"
+                            )
+
+                        }
+                        Text(modifier = Modifier.align(Alignment.CenterHorizontally),text = "Int", fontSize = 20.sp)
+                        Text(modifier = Modifier.align(Alignment.CenterHorizontally),text = int.toString(), fontSize = 20.sp)
+                        Button(onClick = { int-- }) {
+                            Image(
+                                painter = painterResource(com.example.a527lablearnandroid.R.drawable.outline_arrow_downward_24)
+                                , contentDescription = "down"
+                            )
+
+                        }
+                    }
+                }
             }
         }
     }
-}
 
-@Composable
-fun MainScreen() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Gray)
-            .padding(32.dp)
-    ) {
 
-        // HP Bar
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(32.dp)
-                .background(Color.White)
-                .padding(4.dp)
-        ) {
-            Text(
-                text = "HP",
-                modifier = Modifier
-                    .align(Alignment.CenterStart)
-                    .fillMaxWidth(0.5f)
-                    .background(Color.Red)
-                    .padding(8.dp),
-                color = Color.White
-            )
-        }
-
-        // image (เอาไว้ต่อ)
-        Spacer(modifier = Modifier.height(16.dp))
-
-        // status (เอาไว้ต่อ)
-        Text(text = "Status", color = Color.White)
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun MainPreview() {
-    _527LabLearnAndroidTheme {
-        MainScreen()
-    }
 }
